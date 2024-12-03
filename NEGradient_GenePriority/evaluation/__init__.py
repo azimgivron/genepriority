b2ade9815ba10ec7614932aa1ef4f31bd056d340
azimgivron <azimgivron@gmail.com>
2024-12-03 21:19:09 +0100

    update-name

--- NEGradient_GenePriority/evaluation/__init__.py
+++ NEGradient_GenePriority/evaluation/__init__.py
@@ -1,5 +1,5 @@
 # pylint: disable=C0114
 from .evaluation import Evaluation
-from .evaluation_methods import extract_results, train_and_test
+from .compute import extract_results, train_and_test
 from .metrics import bedroc_score
 from .results import Results
