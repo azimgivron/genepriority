a7dd36889cb2d457a388dc58985ecbdf04aa5bca
azimgivron <azimgivron@gmail.com>
2024-12-07 18:40:44 +0100

    update

--- NEGradient_GenePriority/preprocessing/__init__.py
+++ NEGradient_GenePriority/preprocessing/__init__.py
@@ -1,5 +1,6 @@
 # pylint: disable=C0114
 from .indices import Indices
 from .preprocessing import *
+from .side_information_loader import SideInformationLoader
 from .train_test_indices import TrainTestIndices
 from .utils import from_indices
